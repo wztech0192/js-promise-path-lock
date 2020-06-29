ab909882fae4d973671e3bfd70aa9bd9cf240cda
wei zheng <weijie0191@gmail.com>
2020-06-29 17:53:41 -0400

    fix issue

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 export default class PromisePathLock {
-    construcotr(promise, pathname = window.location.pathname) {
+    static PathUID = new (class {
+        _uid = 0;
+        _currPath = "";
+        get() {
+            return `${this._currPath}_$$${this._uid}`;
+        }
+
+        generate() {
+            if (window.location.pathname !== this._currPath) {
+                this._uid++;
+                this._currPath = window.location.pathname;
+            }
+            return this.get();
+        }
+
+        isEqual(target) {
+            return target === this.get();
+        }
+    })();
+
+    constructor(promise, path = { id: PromisePathLock.PathUID.generate() }) {
         this.promise = promise;
-        this.pathname = pathname;
+        this.path = path;
     }
 
     static lock = (promise) => new PromisePathLock(promise);
 
     generateCallback = (callback) => (res) => {
-        if (!callback || this.pathname !== window.location.pathname) {
-            return res;
+        if (callback && PromisePathLock.PathUID.isEqual(this.path.id)) {
+            callback(res);
+            this.path.id = PromisePathLock.PathUID.generate();
         }
-        return callback(res);
     };
 
-    wrapIt = (funcName, ...arg) => new PromisePathLock(this.promise[funcName], this.pathname);
+    wrapIt = (funcName, ...arg) => new PromisePathLock(this.promise[funcName](...arg), this.path);
 
-    then = (onSuccess, onError) =>
-        this.wrapIt("then", this.generateCallback(onSuccess), this.generateCallback(onError));
+    then = (onSuccess) => this.wrapIt("then", this.generateCallback(onSuccess));
 
     catch = (onError) => this.wrapIt("catch", this.generateCallback(onError));
     finally = (onFinally) => this.wrapIt("finally", this.generateCallback(onFinally));
